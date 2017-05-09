445d3d3d72dd406072af29cbcf20e8d8fd7db3a5
Prakash-Balu <prakash.engg89@gmail.com>
2017-05-09 10:45:51 +0530

    register controller file added

---
+++ src/js/controllers/register-ctrl.js
@@ -0,0 +1,50 @@
+/**
+ * Register Controller
+ */
+
+angular.module('RDash')
+    .controller('RegisterCtrl', ['$scope', '$location', '$http', 'AuthServices',  RegisterCtrl]);
+
+function RegisterCtrl($scope, $location, $http, AuthServices) {
+    $scope.users = [];
+    $scope.init = function() {
+        
+    };
+
+    $scope.registerUser = function(userData) {
+        if(userData.password == userData.cpassword) {
+			$scope.users.push({name:userData.name,password:userData.cpassword});
+			$scope.setUsers();
+			console.log($scope.users);
+		}
+    };
+	
+	$scope.getUsers = function() {
+		AuthServices.getUsers()
+		.success(function(data){
+			$scope.users = data;
+		}).error(function(error){
+			alert(error);
+		});
+	};
+	
+	$scope.setUsers = function() {
+		/*AuthServices.setUsers($scope.users)
+		.success(function(data){
+			console.log(data);
+		}).error(function(error){
+			alert(error);
+		});*/
+		
+		$http.put('data/login.json', $scope.users)
+		.success(function(data){
+			 return data;
+		}).error(function(error){
+			alert(error);
+		});
+	};
+
+    //Call function initialization
+	$scope.init();
+	$scope.getUsers();
+}
