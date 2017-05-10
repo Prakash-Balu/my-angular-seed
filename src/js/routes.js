d23f22e13c75a25efced9d26a0b30e3e29ce6adf
Prakash-Balu <prakash.engg89@gmail.com>
2017-05-10 18:46:57 +0530

    updated

--- src/js/routes.js
+++ src/js/routes.js
@@ -12,25 +12,30 @@ angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
         // Application routes
         $stateProvider
 			.state('login', {
+				cache: false,
                 url: '/',
                 templateUrl: 'templates/login.html',
 				controller: 'LoginCtrl'
             })
 			.state('register', {
+				cache: false,
                 url: '/register',
                 templateUrl: 'templates/register.html',
 				controller: 'RegisterCtrl'
             })
 			.state('home', {
+				cache: false,
                 url: '/home',
                 templateUrl: 'templates/home.html',
 				controller: 'MasterCtrl'
             })
 			.state('home.dashboard', {
+				cache: false,
                 url: '/dashboard',
                 templateUrl: 'templates/dashboard.html'
             })
             .state('home.tables', {
+				cache: false,
                 url: '/tables',
                 templateUrl: 'templates/tables.html'
             });
